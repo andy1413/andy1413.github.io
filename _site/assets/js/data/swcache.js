bdd8fbccea2d2f5364af9fbad741d1c698ff9a8f
andy <wangfangshuai@gmail.com>
2022-11-27 16:40:44 +0800

    Update

--- _site/assets/js/data/swcache.js
+++ _site/assets/js/data/swcache.js
@@ -26,17 +26,26 @@ const resource = [
         '/assets/img/favicons/android-chrome-192x192.png',
         '/assets/img/favicons/android-chrome-512x512.png',
         '/assets/img/favicons/apple-touch-icon.png',
+        '/assets/img/favicons/avatar.png',
         '/assets/img/favicons/favicon-16x16.png',
         '/assets/img/favicons/favicon-32x32.png',
         '/assets/img/favicons/favicon.ico',
         '/assets/img/favicons/mstile-150x150.png',
+        '/assets/img/favicons/%E7%94%A8%E6%89%80%E9%80%89%E9%A1%B9%E7%9B%AE%E6%96%B0%E5%BB%BA%E7%9A%84%E6%96%87%E4%BB%B6%E5%A4%B9/android-chrome-192x192.png',
+        '/assets/img/favicons/%E7%94%A8%E6%89%80%E9%80%89%E9%A1%B9%E7%9B%AE%E6%96%B0%E5%BB%BA%E7%9A%84%E6%96%87%E4%BB%B6%E5%A4%B9/android-chrome-512x512.png',
+        '/assets/img/favicons/%E7%94%A8%E6%89%80%E9%80%89%E9%A1%B9%E7%9B%AE%E6%96%B0%E5%BB%BA%E7%9A%84%E6%96%87%E4%BB%B6%E5%A4%B9/apple-touch-icon.png',
+        '/assets/img/favicons/%E7%94%A8%E6%89%80%E9%80%89%E9%A1%B9%E7%9B%AE%E6%96%B0%E5%BB%BA%E7%9A%84%E6%96%87%E4%BB%B6%E5%A4%B9/favicon-16x16.png',
+        '/assets/img/favicons/%E7%94%A8%E6%89%80%E9%80%89%E9%A1%B9%E7%9B%AE%E6%96%B0%E5%BB%BA%E7%9A%84%E6%96%87%E4%BB%B6%E5%A4%B9/favicon-32x32.png',
+        '/assets/img/favicons/%E7%94%A8%E6%89%80%E9%80%89%E9%A1%B9%E7%9B%AE%E6%96%B0%E5%BB%BA%E7%9A%84%E6%96%87%E4%BB%B6%E5%A4%B9/favicon.ico',
+        '/assets/img/favicons/%E7%94%A8%E6%89%80%E9%80%89%E9%A1%B9%E7%9B%AE%E6%96%B0%E5%BB%BA%E7%9A%84%E6%96%87%E4%BB%B6%E5%A4%B9/mstile-150x150.png',
         '/assets/js/dist/categories.min.js',
         '/assets/js/dist/commons.min.js',
         '/assets/js/dist/home.min.js',
         '/assets/js/dist/misc.min.js',
         '/assets/js/dist/page.min.js',
         '/assets/js/dist/post.min.js',
-        '/assets/js/dist/pvreport.min.js'
+        '/assets/js/dist/pvreport.min.js',
+        '/assets/img/favicons/.DS_Store'
 ];
 
 /* The request url with below domain will be cached */
--- _site/sw.js
+++ _site/sw.js
@@ -1,6 +1,6 @@
 self.importScripts('/assets/js/data/swcache.js');
 
-const cacheName = 'chirpy-20221127.133740';
+const cacheName = 'chirpy-20221127.164017';
 
 function verifyDomain(url) {
     for (const domain of allowedDomains) {
