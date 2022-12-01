5b25c14227139ea82d46f341bd3868715288dabf
andy <wangfangshuai@gmail.com>
2022-12-01 17:23:36 +0800

    fix bug

--- _site/sw.js
+++ _site/sw.js
@@ -1,6 +1,6 @@
 self.importScripts('/assets/js/data/swcache.js');
 
-const cacheName = 'chirpy-20221201.163635';
+const cacheName = 'chirpy-20221201.172316';
 
 function verifyDomain(url) {
     for (const domain of allowedDomains) {
