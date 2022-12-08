2dc72eab9bd3a05e96b98aef14a5f5a5c2976e49
andy <wangfangshuai@gmail.com>
2022-12-08 21:58:07 +0800

    update

--- _site/sw.js
+++ _site/sw.js
@@ -1,6 +1,6 @@
 self.importScripts('/assets/js/data/swcache.js');
 
-const cacheName = 'chirpy-20221208.184602';
+const cacheName = 'chirpy-20221208.215754';
 
 function verifyDomain(url) {
     for (const domain of allowedDomains) {
