502512e23e6c830e6f9309ec9f51af67d295615c
andy <wangfangshuai@gmail.com>
2022-12-12 18:49:11 +0800

    Update IAP内购

--- _site/sw.js
+++ _site/sw.js
@@ -1,6 +1,6 @@
 self.importScripts('/assets/js/data/swcache.js');
 
-const cacheName = 'chirpy-20221211.144602';
+const cacheName = 'chirpy-20221212.184853';
 
 function verifyDomain(url) {
     for (const domain of allowedDomains) {
